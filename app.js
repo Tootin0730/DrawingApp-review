f5779e03ec99b4c09c7fdb47714385570f93e4de
Tootin0730 <yisan1004@gmail.com>
2022-12-29 17:18:59 -0500

    #1.5 Drawing Project Two
    Drawing a Person.

--- app.js
+++ app.js
@@ -3,15 +3,15 @@ const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
 
-ctx.fillRect(200, 200, 50, 200);
-ctx.fillRect(400, 200, 50, 200);
-ctx.lineWidth = 2;
-ctx.fillRect(300, 300, 50, 100);
-ctx.fillRect(200, 200, 200, 20);
-ctx.moveTo(200, 200);
-ctx.lineTo(325, 100);
-ctx.lineTo(450, 200);
+ctx.fillRect(200, 200, 20, 100);
+ctx.fillRect(240, 200, 100, 200);
+ctx.fillRect(360, 200, 20, 100);
+ctx.arc(290, 130, 60, 0, 2 * Math.PI);
 ctx.fill();
 
-
+ctx.beginPath();
+ctx.fillStyle = "white";
+ctx.arc(310, 110, 8, Math.PI, 2 * Math.PI);
+ctx.arc(270, 110, 8, Math.PI, 2 * Math.PI);
+ctx.fill();
 
