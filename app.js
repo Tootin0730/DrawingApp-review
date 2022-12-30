b879d520049ea2453f1d86fedf5388b8433708bd
Tootin0730 <yisan1004@gmail.com>
2022-12-30 16:40:01 -0500

    #2.3Paint Color part One
    Changing Color

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const color = document.getElementById("color");
 const lineWidth = document.getElementById("line-width");
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
@@ -25,12 +26,16 @@ function cancelPainting() {
 }
 
 function onLineWidthChange(event) {
-
   ctx.lineWidth = event.target.value;
 }
 
+function onColorChange(event) {
+  ctx.strokeStyle = event.target.value;
+  ctx.fillStyle = event.target.value;
+}
 canvas.addEventListener("mousemove", onMove);
 canvas.addEventListener("mousedown", startPainting);
 canvas.addEventListener("mouseup", cancelPainting);
 canvas.addEventListener("mouseleave", cancelPainting);
 lineWidth.addEventListener("change", onLineWidthChange);
+color.addEventListener("change", onColorChange);
